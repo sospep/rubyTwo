06a8f109f52cdd6e3711da5bbeaf74b1d127392a
Don Sagrott <don.sagrott@gmail.com>
2019-09-04 21:51:00 -0700

    HOUSEKEEPING - Counts - cleanup extra code and comments

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -1,69 +1,26 @@
 class CountsController < ApplicationController
     
-    # require 'builder'
-
     def index
+        # query to execute , NEED to be modified to restrict to last 365 days(see example) but issue with current db (sqlite) date functions
+        @countsSQL =  "select DATE(created_at) as created, count(id) FROM balloons GROUP BY created;"
+        # need something like
         # @countsSQL = "select created_at, count(id) FROM balloons WHERE created_at >= dateadd(day, datediff(day,0,GetDate())- 365,0) GROUP BY created_at;"
-        @countsSQL = "select DATE(created_at) as created, count(id) FROM balloons GROUP BY created;"
-
-        # @counts = ActiveRecord::Base.connection.execute("countsSQL");
+        # executing the query
         @countBalloons = ActiveRecord::Base.connection.exec_query(@countsSQL);
-
-=begin
-        # myXML ="test string 2" // PASS
-        myXML ="<xml><balloons codeA='101' status_code='ebutuoy'>
-        <balloon_counts>
-            <count> 4 </count>
-        </balloon_counts>
-        </balloons>
-        </xml>"
-
-        # File.open("logg.txt", "w") { |f| f.write "#{Time.now} - User logged in\n" }
-        # File.write("myXml.txt", myXML)
-        # puts myXML
-
-        # render plain: myXML // PASS
-
-        # render xml: myXML // PASS -v-001 hard coded string
-
-
-        # TRY-00n
-        myXML ="<xml><balloons codeA='101' status_code='ebutuoy'>
-        <balloon_counts>
-          <% @countBalloons.each do | balloon | %>
-            <count><%= balloon %></count>
-          <%end%>
-        </balloon_counts>
-        </balloons>
-        </xml>"
-        render xml: myXML
-=end
-        # TRY-00n - concatenate string values
+       
+        # TRY-001 - build XML content by concatenating string values, see ALSO TRY-002 - pass into xml template view and TRY-003 - using XML builder VS string concatenation
         myXMLprefix ="<xml><balloons codeA='101' status_code='ebutuoy'>
         <balloon_counts>"
         myXMLsuffix ="</balloon_counts></balloons></xml>"
         myXMLcontent =""
-        
+
         @countBalloons.each do | balloon |
-            # PASS works
-            # myXMLcontent += "<count>" +  balloon["created"] + "</count>"
-
-            # modify to get actual desired output format
-            # myXMLcontent += "<count date='" + balloon["created"] +"'">" +  balloon["count(id)"] + "</count>" // FAIL 
-            # PASS
-            # myXMLcontent += "<count date='" + balloon["created"] +"'> 3 </count>"
-            # PASS - final  
             @thisCount = balloon["count(id)"].to_s
-            myXMLcontent += "<count date='" + balloon["created"] +"'>"+ @thisCount  +"</count>"
-            # FAIL 
-            # myXMLcontent += "<count date='" + balloon["created"] +"'>"+ @balloon["count(id)"].to_s  +"</count>" 
-
+            myXMLcontent += "<count date='" + balloon["created"] +"'>"+ @thisCount +"</count>"
         end
+        # put it all together
         myXML = myXMLprefix + myXMLcontent + myXMLsuffix
+        # sent it the brower as xml
         render xml: myXML
-
-        # TRY next = use /view template VS string builder method 
-        # render xml:@countBalloons
-        # render template: 'views/counts/index.xml.erb'
     end 
 end
