42767e737e543d3a63a4a259bb00485d6c8967dc
Don Sagrott <don.sagrott@gmail.com>
2019-09-14 11:55:27 -0700

    initial commit of example tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get page about" do
+    get about_url #// wrong # 
+    assert_response :success
+  end
 end
